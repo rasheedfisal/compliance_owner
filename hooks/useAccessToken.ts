76e60cc00e87c2dfb8fbaa5a288ea8757f44cfea
DELL <rasheed.fisal2015@gmail.com>
2023-07-20 20:11:53 +0200

    first commit75

--- hooks/useAccessToken.ts
+++ hooks/useAccessToken.ts
@@ -5,7 +5,6 @@ const useAccessToken = () => {
   const stateContext = useStateContext();
   const accessToken = async () => {
     try {
-      console.log("first");
       const { data, message } = await getProfileFn();
       stateContext.dispatch({ type: "SET_USER", payload: data });
 
