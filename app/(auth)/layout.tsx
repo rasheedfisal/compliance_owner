0d8e2a69d65ef106c3bcc0d55bfbd46a32fe9dde
DELL <rasheed.fisal2015@gmail.com>
2023-07-21 15:58:29 +0200

    first commit90

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,6 +5,7 @@ import useUpdateEffect from "@/hooks/useUpdateEffect";
 import { useLoaded } from "@/hooks/useLoaded";
 import { BounceLoader } from "react-spinners";
 import { ToastContainer } from "react-toastify";
+import { setColors } from "../ThemeSettings";
 
 export default function RootLayout({
   children,
@@ -19,32 +20,32 @@ export default function RootLayout({
     // setTheme(isDark as unknown as string);
   };
   const [color, setColor] = useLocalStorage("color", "cyan");
-  const setColors = (color: string) => {
-    const root = document.documentElement;
-    root.style.setProperty("--color-primary", `var(--color-${color})`);
-    root.style.setProperty("--color-primary-50", `var(--color-${color}-50)`);
-    root.style.setProperty("--color-primary-100", `var(--color-${color}-100)`);
-    root.style.setProperty(
-      "--color-primary-light",
-      `var(--color-${color}-light)`
-    );
-    root.style.setProperty(
-      "--color-primary-lighter",
-      `var(--color-${color}-lighter)`
-    );
-    root.style.setProperty(
-      "--color-primary-dark",
-      `var(--color-${color}-dark)`
-    );
-    root.style.setProperty(
-      "--color-primary-darker",
-      `var(--color-${color}-darker)`
-    );
-    // setSelectedColor(color);
-    // window.localStorage.setItem("color", color);
-    setColor(color);
-    //
-  };
+  // const setColors = (color: string) => {
+  //   const root = document.documentElement;
+  //   root.style.setProperty("--color-primary", `var(--color-${color})`);
+  //   root.style.setProperty("--color-primary-50", `var(--color-${color}-50)`);
+  //   root.style.setProperty("--color-primary-100", `var(--color-${color}-100)`);
+  //   root.style.setProperty(
+  //     "--color-primary-light",
+  //     `var(--color-${color}-light)`
+  //   );
+  //   root.style.setProperty(
+  //     "--color-primary-lighter",
+  //     `var(--color-${color}-lighter)`
+  //   );
+  //   root.style.setProperty(
+  //     "--color-primary-dark",
+  //     `var(--color-${color}-dark)`
+  //   );
+  //   root.style.setProperty(
+  //     "--color-primary-darker",
+  //     `var(--color-${color}-darker)`
+  //   );
+  //   // setSelectedColor(color);
+  //   // window.localStorage.setItem("color", color);
+  //   setColor(color);
+  //   //
+  // };
   useUpdateEffect(() => {
     setColors(color);
   }, []);
